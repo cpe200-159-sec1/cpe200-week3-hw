4efc8ee87b5dbe051ef68e28a3f9b2710abb82bd
Potsawat <potsawat_3232@hotmail.com>
2016-09-11 20:23:33 +0700

    Student Fixed Course Done

--- src/cpe200/Student.java
+++ src/cpe200/Student.java
@@ -1,30 +1,21 @@
 package cpe200;
 
 public class Student {
-    String name, studentID;
-    int year;
-    boolean status;
+    private String name, studentID;
+    private int year;
+    private boolean status;
 
     //Constructor
     public Student() {
-        setName("John Doe");
-        setStudent_id("560610000");
-        setYearOfBirth(1990);
-        setStatus(false);
+        this("John Doe", "560610000", 1990, false);
     }
 
     public Student(String n, String id) {
-        setName(n);
-        setStudent_id(id);
-        setYearOfBirth(1990);
-        setStatus(false);
+        this(n, id, 1990, false);
     }
 
     public Student(String n, String id, int yob) {
-        setName(n);
-        setStudent_id(id);
-        setYearOfBirth(yob);
-        setStatus(false);
+        this(n, id, yob, false);
     }
 
     public Student(String n, String id, int yob, boolean s) {
@@ -43,14 +34,14 @@ public class Student {
     public void setStudent_id(String id) {
         if (isValidStudent_id(id))
             studentID = id;
-        if (studentID == null)
+        else if (studentID == null)
             studentID = "560610000";
     }
 
     public void setYearOfBirth(int yob) {
         if (isValidYOB(yob))
             year = yob;
-        if (year == 0)
+        else if (year == 0)
             year = 1990;
     }
 
