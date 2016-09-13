89b7155a854d8eb43d370f4bb1639618acc5beb9
kawewut <kawewut.chu@gmail.com>
2016-09-13 12:37:13 +0700

    580610616

---
+++ lab3-2/src/cpe200/User.java
@@ -0,0 +1,49 @@
+package cpe200;
+
+/**
+ * Created by pruet on 6/9/2559.
+ */
+public class User {
+    protected String userName;
+    protected String password;
+
+    public User()
+    {
+        /* your code here */
+    }
+
+    public boolean setUserName(String name)
+    {
+        String re = "^[A-Za-z][A-Za-z0-9]{7,}$";
+        if(name.matches(re)){
+            userName = name;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setPassword(String name)
+    {
+        /* your code here */
+        String re = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{12,}$";
+        if(name.matches(re)){
+            password = name;
+            return true;
+        }
+        return false;
+
+    }
+
+    public String getUserName()
+    {
+        /* your code here */
+
+        return userName;
+    }
+
+    public String getPassword()
+    {
+        /* your code here */
+        return password;
+    }
+}
