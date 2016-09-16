7d702d87ccd1cf71b51b6fbdce31305de39cd9db
Eakkaphop Wongsorn <eakkaphop.w@gmail.com>
2016-09-16 13:08:33 +0700

    Update Student.java

--- src/cpe200/Student.java
+++ src/cpe200/Student.java
@@ -1,28 +1,95 @@
-package cpe200;
-
 public class Student {
 
+    private String nameOfStudent;
+    private String idOfStudent;
+    private int yearOfBirth;
+    private boolean active;
+
+
     public Student() {
-        // implement here
+
+        nameOfStudent = "John Doe";
+        idOfStudent = "560610000";
+        yearOfBirth = 1990;
+        active = false;
+
     }
+    public Student(String name,String studentId){
+        this();
+        setName(name);
+        setStudent_id(studentId);
+    }
+    public Student(String name,String studentId,int yearOfBirth){
+        this();
+        setName(name);
+        setStudent_id(studentId);
+        setYearOfBirth(yearOfBirth);
+    }
+    public Student(String name,String studentId,int yearOfBirth,boolean active){
+        this();
+        setName(name);
+        setStudent_id(studentId);
+        setYearOfBirth(yearOfBirth);
+        setActive(active);
+    }
+
+
 
     // implement all missing constructors here
 
-    // implement all get and set methods here
+    public void setStudent_id(String idOfStudent) {
+        if(isValidStudent_id(idOfStudent))
+            this.idOfStudent = idOfStudent;
+    }
 
-    @Override
+    public void setName(String nameOfStudent) {
+        if(nameOfStudent.length() > 0)
+            this.nameOfStudent = nameOfStudent;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if(isValidYOB(yearOfBirth))
+            this.yearOfBirth = yearOfBirth;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getName() {
+        return nameOfStudent;
+    }
+
+    public String getStudent_id() {
+
+        return idOfStudent;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+        @Override
     public String toString() {
-        String o = "Something";
+        String o = String.format("%s (%s) was born in %d is an %s student.",getName(),getStudent_id(),getYearOfBirth(),(isActive())?"ACTIVE":"INACTIVE");
 
         return o;
     }
 
     private boolean isValidStudent_id(String id) {
-        return true;
+
+        String reg_ex = "^5[6-9]061[0-2][0-9]{3}$";
+        return id.matches(reg_ex);
     }
 
     private boolean isValidYOB(int yob) {
-        return true;
+        if(yob > 1986)
+            return true;
+        else return false;
     }
 
     // declare your attributes here
