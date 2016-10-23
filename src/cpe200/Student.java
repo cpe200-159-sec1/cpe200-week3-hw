c46fbac33fe02e77a1e6266464a123e7b9a1159e
somprasertTS <ssom.ts@gmail.com>
2016-10-23 21:03:18 +0700

    580610647

--- src/cpe200/Student.java
+++ src/cpe200/Student.java
@@ -26,22 +26,54 @@ public class Student {
         this.year = isValidYOB(yob)?yob:1990;
         this.status =stat;
     }
+    public void setName(String n){
+        if(n.length()>0){
+            this.stdname=n;
+        }
+    }
+
+    public  void setStudent_id(String id){
+        if(isValidStudent_id(id)){
+            this.stdid=id;
+        }
+    }
+
+    public void setYearOfBirth(int yob){
+        if (isValidYOB(yob)){
+            this.year=yob;
+        }
+    }
+
+    public String getName(){
+        return this.stdname;
+    }
+
+    public String getStudent_id(){
+        return this.stdid;
+    }
+
+    public int getYearOfBirth(){
+        return this.year;
+    }
 
     public boolean isActive(){
         return this.status;
     }
     @Override
     public String toString() {
-        String o = this.stdname+" ("+this.stdid+") was born in"+this.year+"is an"+(isActive()?"Active":"Not active")+"student.";
+        String o = this.stdname+" ("+this.stdid+") was born in "+this.year+" is an "+(isActive()?"ACTIVE":"INACTIVE")+" student.";
         return o;
     }
 
     private boolean isValidStudent_id(String id) {
-        return true;
+        String Check = "[5][6-9][0][6][1][0-2][0-9][0-9][0-9]";
+        if (id.matches(Check)) return true;
+        else  return false;
     }
 
     private boolean isValidYOB(int yob) {
-        return true;
+        if (yob >= 1989) return true;
+        else return false;
     }
 
     // declare your attributes here
